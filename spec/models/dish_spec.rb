94357eac4bc2f2bd7417455abb92a63679f8649e
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-20 06:58:14 -0600

    Add model tests and method for dish.total_calories

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,22 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe "methods" do
+    let!(:marcos) { Chef.create!(name: "Marcos") }
+    let!(:mapo) { marcos.dishes.create(name: "Mapo Tofu", description: "Fire and Tingle") }
+    let!(:pork) { Ingredient.create!(name: "pork", calories: 130) }
+    let!(:gochujang) { Ingredient.create!(name: "gochujang", calories: 30) }
+    let!(:tofu) { Ingredient.create!(name: "tofu", calories: 60) }
+    let!(:pepper) { Ingredient.create!(name: "black pepper", calories: 2) }
+
+    it "#total_calories" do
+      mapo.ingredients << pork
+      mapo.ingredients << pepper
+      mapo.ingredients << tofu
+      mapo.ingredients << gochujang
+
+      expect(mapo.total_calories).to eq(222)
+    end
+  end
 end
