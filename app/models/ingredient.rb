9d30a7e4d4d7a79ff0849eb5eb9c946118269847
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-20 06:18:27 -0600

    Add model relationships and requisite tests

---
+++ app/models/ingredient.rb
@@ -0,0 +1,9 @@
+class Ingredient < ApplicationRecord
+
+  validates_presence_of :name
+  validates_numericality_of :calories
+
+
+  has_many :dish_ingredients
+  has_many :dishes, through: :dish_ingredients
+end
